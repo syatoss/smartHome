327dff86047210698ad02e1d77e73a050d4244ce
stas <syatoss@gmail.com>
2019-12-30 20:57:09 +0200

    Updates

---
+++ precache-manifest.b0a58f0ca2f0ecc5beffdd35c3a58e36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8d6866b0567b3d68d9c7c78968defb",
+    "url": "/smartHome/index.html"
+  },
+  {
+    "revision": "8dea8fe91e272e9bdafd",
+    "url": "/smartHome/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ee86c726d7cbb5899253",
+    "url": "/smartHome/static/css/main.7e506dba.chunk.css"
+  },
+  {
+    "revision": "8dea8fe91e272e9bdafd",
+    "url": "/smartHome/static/js/2.cf347e57.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/smartHome/static/js/2.cf347e57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee86c726d7cbb5899253",
+    "url": "/smartHome/static/js/main.dafe5ef9.chunk.js"
+  },
+  {
+    "revision": "252baf565b3132e4d809",
+    "url": "/smartHome/static/js/runtime-main.ad304b41.js"
+  }
+]);
